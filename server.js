03abd6ea34b199d1a7aa20666fba5101ccd3e3f2
akas <test090503@gmail.com>
2020-10-12 23:14:36 +0600

    mongoDB databse conneciton

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express')
 
 const app = express()
 
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+
 const port = process.env.port || 8000
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost/users',{useNewUrlParser:true},() => {
+    console.log('MongoDB databse connection successful')
+})
 
 app.listen(port ,()=> {
     console.log(`App is connected on port ${port}`)
